e19f17c61722697d1bc06f4404261b7e9cff6f69
krups88 <b88pl88@gmail.com>
2023-01-18 21:44:50 +0100

    task 3

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 /* Bohdan*/
-function boolToWord( bool ){
-    if (bool === true) {return "Yes"}
-else { return "No"}
+/*1. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    return number % 2 ? "Odd" : "Even"
 }
+evenOrOdd(5)
+console.log(evenOrOdd(5))*/
 
-/*
-function boolToWord( bool ){
-    return bool ? 'Yes':'No';
-  } /*
+/*2.Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+const min = (list) => Math.min (...list)
+    
+const max = (list) => Math.max (...list)
 
+console.log(min([12 , 20 ,50 , -50 , 25 ,111]))
+console.log(max([12 , 20 ,50 , -50 , 25 ,111]))*/
 
-/*console.log(booleanToString);
-
-
+3.
